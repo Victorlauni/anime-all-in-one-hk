e35fa7b10e83f1d53e741573cd9cac0b262ecee1
Victor Lau <vtleluc@gmail.com>
2021-06-10 00:48:47 +0800

    Firebase config

--- src/api/common.js
+++ src/api/common.js
@@ -1,8 +1,13 @@
+import { db } from "../lib/firebase"
+
 export const getAllSession = async () => {
+  let result = db.collection('season').orderBy('order', 'desc').get().then(docs => {
+    return docs.docs.map(doc => doc.id.replaceAll('_', " "))
+  })
   const mock = {
     data: ["2020 Winter", "2021 Spring", "2021 Summer"].reverse()
   }
-  return Promise.resolve(mock.data)
+  return Promise.resolve(result)
 }
 
 export const getWatchingBangumi = async () => {
