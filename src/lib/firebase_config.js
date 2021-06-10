acc84b82557ca00f6c84919ac5ec46e3b4d4a5bd
Victor Lau <vtleluc@gmail.com>
2021-06-10 20:45:37 +0800

    add firebase token

---
+++ src/lib/firebase_config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAcyCwXweLLloCKAlAEardbshaN5M-jlmI",
+  authDomain: "anime-all-in-one.firebaseapp.com",
+  databaseURL: "https://anime-all-in-one.firebaseio.com",
+  projectId: "anime-all-in-one",
+  storageBucket: "anime-all-in-one.appspot.com",
+  messagingSenderId: "690270566110",
+  appId: "1:690270566110:web:ebe189da478b15576c344f",
+  measurementId: "G-GZQ3QV16CY"
+};
